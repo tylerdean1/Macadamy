621ec6cd61c2381a175c0977be4f8e70dba5e565
Tyler Dean <tyler.adean93@yahoo.com>
2025-04-10 23:52:09 -0700

    Remove unused CSS import from ContractDashboard component

--- src/pages/ContractDashboard.tsx
+++ src/pages/ContractDashboard.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import '../styles/ContractDashboard.css';
 import { useParams, useNavigate } from 'react-router-dom';
 import {
   Truck,
